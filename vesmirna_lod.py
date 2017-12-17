53733733cbd59339a44d34df9f59ea859ce485ff
Štěpán Tomsa <stepantomsa@icloud.com>
2017-12-17 12:21:07 +0100

    Oprav hodiny
    Metoda movevent musí mít jako parametr časový rozdíl z funkce
    tick. Zároveň nedává smysl volat tuto metodu při stisku klávesy.

--- vesmirna_lod.py
+++ vesmirna_lod.py
@@ -28,7 +28,7 @@ class Spaceship:
         self.y = y
 
 
-    def movement(self):
+    def movement(self, dt):
         self.x = self.x + dt * self.x_speed
         self.y = self.y + dt * self.y_speed
         self.rotation = self.rotation + dt * self.rotation_speed
@@ -42,13 +42,12 @@ class Spaceship:
 
 ship = Spaceship()
 
-def tick(t):
-    ship.movement()
+def tick(dt):
+    ship.movement(dt)
 
 pyglet.clock.schedule_interval(tick, 1/30)
 
 window.push_handlers(
-    on_text=ship.movement,
     on_draw=ship.vykresli,
     on_mouse_press=ship.klik,
 )
